273217db1cc874a16be59a6836e3c3b6d7e04509
Muhaimin CS <muhaimincs@gmail.com>
2021-01-25 02:40:36 +0800

    configure title and description

--- src/pages/index.js
+++ src/pages/index.js
@@ -59,8 +59,8 @@ function Home() {
   const { siteConfig = {} } = context;
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`👋🏼 ${siteConfig.title}`}
+      description="Netlify CMS Guide for Billie Trust Ltd ">
       <header className={clsx('hero hero--primary', styles.heroBanner)}>
         <div className="container">
           <h1 className="hero__title">{siteConfig.title}</h1>
